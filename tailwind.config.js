cdf540ac74da6e929153e6471693707e4fa4dbf4
Alessandro Chinchilla <ale030401@gmail.com>
2024-09-24 14:36:27 -0600

    Add:custom fonts and color vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				custom1: ["CustomFont1", "sans-serif"],
+				custom2: ["CustomFont2", "sans-serif"],
+			},
+			colors: {
+				redPrimary: "var(--red-primary)",
+				redSecondary: "var(--red-secondary)",
+				bluePrimary: "var(--blue-primary)",
+				blueSecondary: "var(--blue-secondary)",
+				bgWhite: "var(--bg-white)",
+			},
+		},
 	},
 	plugins: [],
 };

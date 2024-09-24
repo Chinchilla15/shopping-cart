e3a1ab5a3da3b76a03babaa22fa1b45aba8a4c07
Alessandro Chinchilla <ale030401@gmail.com>
2024-09-24 14:43:39 -0600

    Update: styles and icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCartShopping,
+	faRecordVinyl,
+	faSun,
+} from "@fortawesome/free-solid-svg-icons";
+
 type HeaderProps = {
 	title: string;
 };
 
-const Header: React.FC<HeaderProps> = ({ title }) => {
+const Header = ({ title }: HeaderProps) => {
 	return (
-		<header className="max-w-full py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-			<h1 className="text-3xl font-bold text-gray-900">
-				<Link to="/" className=" hover:text-gray-900">
+		<header className="max-w-full py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center bg-blueSecondary">
+			<h1 className="text-3xl font-bold font-custom2 text-gray-900">
+				<Link to="/" className="hover:text-gray-900 pr-1">
 					{title}
 				</Link>
+				<FontAwesomeIcon icon={faRecordVinyl} size="lg" />
 			</h1>
 			<nav>
 				<ul className="flex space-x-4">
@@ -20,6 +25,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
 						<Link
 							to="/"
 							className="text-gray-700 hover:text-gray-900"
+							title="Home"
 						>
 							Home
 						</Link>
@@ -28,6 +34,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
 						<Link
 							to="/products"
 							className="text-gray-700 hover:text-gray-900"
+							title="Products"
 						>
 							Products
 						</Link>
@@ -36,6 +43,7 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
 						<Link
 							to="/about"
 							className="text-gray-700 hover:text-gray-900"
+							title="About"
 						>
 							About
 						</Link>
@@ -51,6 +59,15 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
 								title="Shopping Cart"
 							/>
 						</Link>
+					</li>
+					<li>
+						<button className="bg-transparent text-gray-700 hover:text-gray-900">
+							<FontAwesomeIcon
+								icon={faSun}
+								size="xl"
+								title="Toggle Theme"
+							/>
+						</button>
 					</li>
 				</ul>
 			</nav>

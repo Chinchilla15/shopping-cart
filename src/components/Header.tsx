91e65ab11a5adbf9db4b09e0b8d10168386e6edd
Alessandro Chinchilla <ale030401@gmail.com>
2024-09-25 16:15:33 -0600

    Add: header compnent color change on scroll; Button component conditional rendering adding link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,53 +5,82 @@ import {
 	faRecordVinyl,
 	faSun,
 } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
 
 type HeaderProps = {
 	title: string;
 };
 
 const Header = ({ title }: HeaderProps) => {
+	const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (window.scrollY > 0) {
+				setIsScrolled(true);
+			} else {
+				setIsScrolled(false);
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
 	return (
-		<header className="max-w-full py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center bg-blueSecondary">
-			<h1 className="text-3xl font-bold font-custom2 text-gray-900">
-				<Link to="/" className="hover:text-gray-900 pr-1">
+		<header
+			className={`sticky top-0 z-30 max-w-full py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center transition-colors duration-300 ${
+				isScrolled
+					? "bg-white text-bluePrimary"
+					: "bg-bluePrimary text-bgWhite"
+			}`}
+		>
+			<h1
+				className={`text-3xl font-bold font-custom2 ${
+					isScrolled
+						? "text-bluePrimary hover:text-redSecondary"
+						: "text-bgWhite hover:text-redSecondary"
+				}`}
+			>
+				<Link to="/" className="hover:text-redSecondary">
 					{title}
+					<FontAwesomeIcon
+						icon={faRecordVinyl}
+						size="lg"
+						className="pl-1"
+					/>
 				</Link>
-				<FontAwesomeIcon icon={faRecordVinyl} size="lg" />
 			</h1>
 			<nav>
 				<ul className="flex space-x-4">
-					<li>
-						<Link
-							to="/"
-							className="text-gray-700 hover:text-gray-900"
-							title="Home"
-						>
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/products"
-							className="text-gray-700 hover:text-gray-900"
-							title="Products"
-						>
-							Products
-						</Link>
-					</li>
-					<li>
-						<Link
-							to="/about"
-							className="text-gray-700 hover:text-gray-900"
-							title="About"
-						>
-							About
-						</Link>
-					</li>
+					{["Home", "Products", "About"].map((item) => (
+						<li key={item}>
+							<Link
+								to={
+									item === "Home"
+										? "/"
+										: `/${item.toLowerCase()}`
+								}
+								className={`${
+									isScrolled
+										? "text-bluePrimary"
+										: "text-bgWhite"
+								} hover:text-redSecondary`}
+								title={item}
+							>
+								{item}
+							</Link>
+						</li>
+					))}
 					<li>
 						<Link
 							to="/cart"
-							className="text-gray-700 hover:text-gray-900"
+							className={`${
+								isScrolled ? "text-bluePrimary" : "text-bgWhite"
+							} hover:text-redSecondary`}
 						>
 							<FontAwesomeIcon
 								icon={faCartShopping}
@@ -61,7 +90,11 @@ const Header = ({ title }: HeaderProps) => {
 						</Link>
 					</li>
 					<li>
-						<button className="bg-transparent text-gray-700 hover:text-gray-900">
+						<button
+							className={`bg-transparent ${
+								isScrolled ? "text-bluePrimary" : "text-bgWhite"
+							} hover:text-redSecondary`}
+						>
 							<FontAwesomeIcon
 								icon={faSun}
 								size="xl"
--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
 			{/* Content overlay */}
 			<div className="absolute inset-0 z-20 flex items-center justify-center">
 				<div className="text-center text-bgWhite px-4 sm:px-6 lg:px-8">
-					<h1 className="text-4xl font-extrabold font-custom1 sm:text-5xl md:text-6xl">
+					<h1 className="text-4xl font-extrabold font-custom1 sm:text-5xl md:text-7xl">
 						Welcome
 					</h1>
 					<p className="mt-3 max-w-md mx-auto text-base sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
@@ -20,7 +20,7 @@ const Hero = () => {
 					</p>
 					<div className="mt-10 sm:flex sm:justify-center">
 						<div className="rounded-md shadow">
-							<Button content="Shop Now" />
+							<Button content="Shop Now" link="/products" />
 						</div>
 					</div>
 				</div>
